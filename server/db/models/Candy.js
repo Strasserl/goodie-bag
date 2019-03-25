8b02363f427c12301064f819f1f587f83f764dcf
Lindsay Strasser <lindsay.strasser@gmail.com>
2019-03-24 21:01:36 -0400

    adds and defines a candy model

--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -2,5 +2,23 @@ const Sequelize = require('sequelize');
 const db = require('../database');
 
 module.exports = db.define('candy', {
-  // define your model here!
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    notEmpty: true,
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    notEmpty: true,
+  },
+  quantity: {
+    isNumeric: true,
+    validate: { max: 10 },
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      'https://images.freeimages.com/images/large-previews/11b/candy-1326928.jpg',
+  },
 });
